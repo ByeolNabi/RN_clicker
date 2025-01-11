80715187e5ca491c14ffee41f57df1b4f99cd4f6
ByeolNabi <narwhal_blast@naver.com>
2025-01-11 19:59:43 +0900

    icon set

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,7 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
 import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
@@ -8,21 +11,21 @@ export default function TabLayout() {
                 name="index"
                 options={{
                     title: 'Cliker',
-                    tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+                    tabBarIcon: ({ color }) => <FontAwesome size={28} name="gamepad" color={color} />,
                 }}
             />
             <Tabs.Screen
                 name="ranking"
                 options={{
                     title: 'Ranking',
-                    tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+                    tabBarIcon: ({ color }) => <FontAwesome6 name="ranking-star" size={24} color="black" />,
                 }}
             />
             <Tabs.Screen
                 name="timer"
                 options={{
                     title: 'Timer',
-                    tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+                    tabBarIcon: ({ color }) => <Ionicons name="timer" size={24} color="black" />,
                 }}
             />
         </Tabs>
